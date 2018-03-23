0eba95b2edefb9eb676ffa760930848c41de1a7d
liyazhou <liyazhou@outlook.com>
2018-03-23 18:31:17 +0800

    builder pattern

--- src/com/anxpp/designpattern/builder/IDateBuilder.java
+++ src/com/anxpp/designpattern/builder/IDateBuilder.java
@@ -1,6 +1,8 @@
 package com.anxpp.designpattern.builder;
+
 //抽象生成器
 public interface IDateBuilder {
-	IDateBuilder buildDate(int y,int m,int d);
-	String date();
+    IDateBuilder buildDate(int y, int m, int d);
+
+    String date();
 }
--- src/com/anxpp/designpattern/builder/MyDate.java
+++ src/com/anxpp/designpattern/builder/MyDate.java
@@ -1,5 +1,6 @@
 package com.anxpp.designpattern.builder;
+
 //产品
 public class MyDate {
-	String date;
+    String date;
 }
