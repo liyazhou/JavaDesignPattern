f3a06110e8354a232bd1d6ec9a263707d71a78d2
liyazhou <liyazhou@outlook.com>
2018-03-26 14:57:35 +0800

    handler

--- src/com/anxpp/designpattern/chainofresponsibility/Handler.java
+++ src/com/anxpp/designpattern/chainofresponsibility/Handler.java
@@ -1,6 +1,8 @@
 package com.anxpp.designpattern.chainofresponsibility;
+
 //处理者
 public interface Handler {
-	int handleRequest(int n);
-	void setNextHandler(Handler next);
+    int handleRequest(int n);
+
+    void setNextHandler(Handler next);
 }
